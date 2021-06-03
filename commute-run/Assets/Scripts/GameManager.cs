34847674f96113833283e6444065a78d0cc3c425
jungsuns2 <dlwjdtjs99@naver.com>
2021-06-04 03:29:35 +0900

    맵, 코인, 장애물 수정

--- commute-run/Assets/Scripts/GameManager.cs
+++ commute-run/Assets/Scripts/GameManager.cs
@@ -4,23 +4,58 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+public static class globalValue
+{
+    public static int score;
+}
+
 public class GameManager : MonoBehaviour
 {
     public int TotalGoldCount;
     public int stage;
     public Text PlayerGoldCntText;
     public Text StageGoldCntText;
+    public Text scoreText;
 
     private void Awake()
     {
-        TotalGoldCount = GameObject.FindGameObjectsWithTag("Gold").Length;
-        StageGoldCntText.text = "/ " +( GameObject.FindGameObjectsWithTag("Gold").Length).ToString();
+        if(TotalGoldCount != null)
+            TotalGoldCount = GameObject.FindGameObjectsWithTag("Gold").Length;
 
+        if (StageGoldCntText != null)
+            StageGoldCntText.text = "/ " +( GameObject.FindGameObjectsWithTag("Gold").Length).ToString();
+
+        if(stage == 3)
+        {
+            Debug.Log("스타트 스테이지");
+            globalValue.score = 0;
+        }
+
+        else if (stage == 4)
+        {
+            Debug.Log("점수 스테이지");
+            scoreText.text = "SCORE : " + globalValue.score;
+        }
         //if(TotalGoldCount == 0)
         //{
         //    TotalGoldCount = GameObject.FindGameObjectsWithTag("Coin").Length;
         //    StageGoldCntText.text = "/ " + (GameObject.FindGameObjectsWithTag("Coin").Length).ToString();
         //}
+    }
+
+    private void Update()
+    {
+        if (stage == 3)
+        {
+            Debug.Log("스타트 스테이지");
+            globalValue.score = 0;
+        }
+
+        else if (stage == 4)
+        {
+            Debug.Log("점수 스테이지");
+            scoreText.text = "SCORE : " + globalValue.score;
+        }
     }
 
     public void GetGold(int count)
